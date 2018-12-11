02c610fa5e6a827a0b39022f49f5390e926d7554
Aaron <aien0322@gmail.com>
2018-12-11 17:27:54 +0800

    add one-shot-learning v1

--- face_utils/urls.py
+++ face_utils/urls.py
@@ -6,4 +6,7 @@ app_name = "face_utils"
 urlpatterns = [
     path('identify_face', views.identify_face),
     path('identify_emotion', views.identify_emotion),
+    path('new_member', views.new_member),
+    path('recognize_face', views.recognize_face),
+    path('get_members', views.get_members),
 ]
--- face_utils/views.py
+++ face_utils/views.py
@@ -7,10 +7,22 @@ from tensorflow.keras.models import load_model
 import base64
 import numpy as np
 import argparse
+from imutils import paths
 import imutils
 import pickle
 import cv2
 import os
+import pymongo
+
+opencv_face_recognition = "opencv-face-recognition"
+
+print("[INFO] loading face detector...")
+protoPath = os.path.join(opencv_face_recognition, "face_detection_model", "deploy.prototxt")
+modelPath = os.path.join(opencv_face_recognition, "face_detection_model", "res10_300x300_ssd_iter_140000.caffemodel")
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+print("[INFO] loading face embedder model...")
+embedder = cv2.dnn.readNetFromTorch(os.path.join(opencv_face_recognition, "openface_nn4.small2.v1.t7"))
 
 def get_square_box(box):
     """Get a square box out of the given box, by expanding it."""
@@ -53,13 +65,13 @@ def base64_to_nparray(base64_str):
     return img
 
 def extract_face(image, square=False):
-    path = 'opencv-face-recognition'
-    CONFIDENCE = 0.5
+    # path = 'opencv-face-recognition'
+    # CONFIDENCE = 0.5
 
-    print("[INFO] loading face detector...")
-    protoPath = os.path.sep.join([path, 'face_detection_model', "deploy.prototxt"])
-    modelPath = os.path.sep.join([path, 'face_detection_model', "res10_300x300_ssd_iter_140000.caffemodel"])
-    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+    # print("[INFO] loading face detector...")
+    # protoPath = os.path.sep.join([path, 'face_detection_model', "deploy.prototxt"])
+    # modelPath = os.path.sep.join([path, 'face_detection_model', "res10_300x300_ssd_iter_140000.caffemodel"])
+    # detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
     (h, w) = image.shape[:2]
     
@@ -79,7 +91,7 @@ def extract_face(image, square=False):
         confidence = detections[0, 0, i, 2]
 
         # filter out weak detections
-        if confidence > CONFIDENCE:
+        if confidence > 0.5:
             # compute the (x, y)-coordinates of the bounding box for the
             # face
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
@@ -166,7 +178,7 @@ def identify_emotion(request):
         imagePath = 'cameraCapture.png'
 
         # TODO: find more efficient way to load model
-        model = load_model(os.path.sep.join([path, 'model-2.h5']))
+        model = load_model(os.path.sep.join([path, 'model_20181130.h5']))
 
         path_ = os.path.sep.join([path, 'test'])
 
@@ -214,3 +226,135 @@ def identify_emotion(request):
             tf.keras.backend.clear_session()
 
         return JsonResponse(response)
+
+def get_face_embedding_vector(imagePaths):
+    for (i, imagePath) in enumerate(imagePaths):
+        print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+        
+        # load the image, resize it to have a width of 600 pixels (while
+        # maintaining the aspect ratio), and then grab the image
+        # dimensions
+        image = cv2.imread(imagePath)
+        image = imutils.resize(image, width=600)
+        (h, w) = image.shape[:2]
+
+        # construct a blob from the image
+        imageBlob = cv2.dnn.blobFromImage(
+            cv2.resize(image, (300, 300)), 1.0, (300, 300),
+            (104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+        # apply OpenCV's deep learning-based face detector to localize
+        # faces in the input image
+        detector.setInput(imageBlob)
+        detections = detector.forward()
+
+        # ensure at least one face was found
+        if len(detections) > 0:
+            # we're making the assumption that each image has only ONE
+            # face, so find the bounding box with the largest probability
+            i = np.argmax(detections[0, 0, :, 2])
+            confidence = detections[0, 0, i, 2]
+
+            # ensure that the detection with the largest probability also
+            # means our minimum probability test (thus helping filter out
+            # weak detections)
+            if confidence > 0.5:
+                # compute the (x, y)-coordinates of the bounding box for
+                # the face
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                # extract the face ROI and grab the ROI dimensions
+                face = image[startY:endY, startX:endX]
+                (fH, fW) = face.shape[:2]
+
+                # ensure the face width and height are sufficiently large
+                if fW < 20 or fH < 20:
+                    continue
+
+                # construct a blob for the face ROI, then pass the blob
+                # through our face embedding model to obtain the 128-d
+                # quantification of the face
+                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+                    (96, 96), (0, 0, 0), swapRB=True, crop=False)
+                embedder.setInput(faceBlob)
+                vec = embedder.forward()
+
+                # add the name of the person + corresponding face
+                # embedding to their respective lists
+                return vec.flatten()
+
+def calc_face_embedding_vector(face):
+    faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+        (96, 96), (0, 0, 0), swapRB=True, crop=False)
+    embedder.setInput(faceBlob)
+    vec = embedder.forward()
+
+    return vec.flatten()
+
+def add_to_embeddings(face_vector, name):
+    client = pymongo.MongoClient()
+    db = client.embeddings
+    myMembers = db.members
+
+    print(type(face_vector))
+
+    member = {
+        'name': name,
+        'vector': face_vector.tolist()
+    }
+    
+    myMembers.insert_one(member)
+    # myMembers.delete_many({'name':'aaron'})
+
+    print(f'added {name} to embeddings')
+
+def new_member(request):
+    print("[INFO] quantifying faces...")
+    from imutils import paths
+    imagePaths = list(paths.list_images(os.path.join(opencv_face_recognition, "extract")))
+    print(f"FOUND {len(imagePaths)} IMAGE(S):", imagePaths)
+
+    face_vector = get_face_embedding_vector(imagePaths)
+    add_to_embeddings(face_vector, 'lydia')
+
+    return HttpResponse(imagePaths)
+
+@csrf_exempt
+def recognize_face(request):
+    if request.method == 'POST':
+        imgString = request.POST['image']
+        img = base64_to_nparray(imgString)
+
+        client = pymongo.MongoClient()
+        db = client.embeddings
+        myMembers = db.members
+
+        try:
+            face, startX, startY, endX, endY = extract_face(img)
+            test_vector = calc_face_embedding_vector(face)
+
+            l2_dist = 100
+            name = 'unknown'
+            for member in myMembers.find():
+                temp_l2_dist = np.sqrt(np.sum(np.square(test_vector - np.array(member['vector']))))
+                print(temp_l2_dist)
+                if temp_l2_dist < l2_dist and temp_l2_dist <= 0.80:
+                    l2_dist = temp_l2_dist
+                    name = member['name']
+            print("min dist.", l2_dist)
+
+            return HttpResponse(f"{name} found!")
+
+def get_members(request):
+    client = pymongo.MongoClient()
+    db = client.embeddings
+    myMembers = db.members
+
+    members = {
+        'names': []
+    }
+    for member in myMembers.find():
+        members['names'].append(member['name'])
+
+    return JsonResponse(members)
